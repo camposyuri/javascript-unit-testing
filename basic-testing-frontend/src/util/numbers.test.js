e128445d9d5e358f81138b40c1883d7c1034cd43
camposyuri <yurinapoleao@gmail.com>
2023-06-27 16:19:30 -0300

    Exercise and Solution

---
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -0,0 +1,43 @@
+import { expect, it } from "vitest";
+import { transformToNumber } from "./numbers";
+
+it("should transform a string value into a number.", () => {
+  // Arrange
+  const convertToNumber = "22";
+
+  // Act
+  const result = transformToNumber(convertToNumber);
+
+  // Asserts
+  const expectedValue = +convertToNumber;
+
+  expect(result).toBe(expectedValue);
+});
+
+it("should transform a boolean value into a number.", () => {
+  const convertToNumber = true;
+
+  const result = transformToNumber(convertToNumber);
+
+  const expectedValue = +convertToNumber;
+
+  expect(result).toBe(expectedValue);
+});
+
+it("should yield NaN if the value contains a character.", () => {
+  const convertToNumber = "invalid number";
+
+  const result = transformToNumber(convertToNumber);
+
+  expect(result).toBeNaN();
+});
+
+// Solution
+
+it("should transform a string number to a number of type of number.", () => {
+  const input = "1";
+
+  const result = transformToNumber(input);
+
+  expect(result).toBeTypeOf("number");
+});
