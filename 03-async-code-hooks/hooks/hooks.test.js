d03b6dde47039b4dd051b0622970e59123d1e416
camposyuri <yurinapoleao@gmail.com>
2023-10-14 13:57:39 -0300

    Using hook beforeAll/beforeEach/afterAll/afterEach

--- 03-async-code-hooks/hooks/hooks.test.js
+++ 03-async-code-hooks/hooks/hooks.test.js
@@ -1,47 +1,67 @@
-import { describe, expect, it } from "vitest";
+import {
+  afterAll,
+  afterEach,
+  beforeAll,
+  beforeEach,
+  describe,
+  expect,
+  it,
+} from "vitest";
 
 import { User } from "./hooks";
 
+/**
+ * * beforeAll() -> Should be executed either before all tests run.
+ * * beforeEach() -> Should be executed before every single test run.
+ * * afterAll() -> Should be executed after all tests executed.
+ * * afterEach() -> Should be executed after every single test.
+ */
+
 describe("hooks", () => {
+  const testEmail = "test@test.com";
+  let user;
+
+  beforeAll(() => {
+    user = new User(testEmail);
+    console.log("beforeAll()");
+  });
+
+  beforeEach(() => {
+    user = new User(testEmail);
+    console.log("beforeEach()");
+  });
+
+  afterAll(() => {
+    console.log("afterAll()");
+  });
+
+  afterEach(() => {
+    console.log("afterEach");
+  });
+
   it("should update the email", () => {
-    const testEmail = "test@test.com";
     const newTestEmail = "test2@test.com";
 
-    const user = new User(testEmail);
     user.updateEmail(newTestEmail);
 
     expect(user.email).toBe(newTestEmail);
   });
 
   it("should have an email property", () => {
-    const testEmail = "test@test.com";
-
-    const user = new User(testEmail);
-
     expect(user).toHaveProperty("email");
   });
 
   it("should store the provided email value", () => {
-    const testEmail = "test@test.com";
-
-    const user = new User(testEmail);
-
     expect(user.email).toBe(testEmail);
   });
 
   it("should clear the email", () => {
-    const testEmail = "test@test.com";
-
-    const user = new User(testEmail);
     user.clearEmail();
 
     expect(user.email).toBe("");
   });
 
   it("should still have an email property after clearing the email", () => {
-    const testEmail = "test@test.com";
-
-    const user = new User(testEmail);
     user.clearEmail();
 
     expect(user).toHaveProperty("email");
